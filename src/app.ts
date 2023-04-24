5284113fea22911435b7410c0263a776742da787
Ray Alcides <railayli2@hotmail.com>
2023-04-24 17:59:24 -0300

    Conectando MongoDB Atlas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 class App {
 public app: express.Application;
@@ -7,6 +8,22 @@ public app: express.Application;
 public constructor() {
   this.app = express();
   this.app.use(cors());
+
+  this.initMongoose();
+  this.connectDatabase();
+}
+
+private initMongoose(): void {
+  mongoose.set('runValidators', true);
+}
+
+private connectDatabase(): void {
+  mongoose.connect('mongodb+srv://rayAlcides03:GaKEoPNNfOaXZKLO@cluster0.81kj61q.mongodb.net/todo-list?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
 }
 
 public listen(port: number): void {
